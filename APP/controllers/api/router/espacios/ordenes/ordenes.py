d90c52273cc4a0d1ed8bea3c8e11236c30e8710a
jarvishuice <jarvis.realg@gmail.com>
2023-12-28 11:41:18 -0400

    d

--- APP/controllers/api/router/espacios/ordenes/ordenes.py
+++ APP/controllers/api/router/espacios/ordenes/ordenes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter,Request,HTTPException,UploadFile,File,Response
 urlBase = "/MUNAY/nest/Espacios"
-Ordenes=APIRouter(prefix=f"{urlBase}/Ordenes", tags=["/Ordenes"])
+Ordenes=APIRouter(prefix=f"/MUNAY/nest/Espacios/Ordenes", tags=["Ordenes"])
 @Ordenes.get("/heloo")
 def hello():
     return {"hello"}
