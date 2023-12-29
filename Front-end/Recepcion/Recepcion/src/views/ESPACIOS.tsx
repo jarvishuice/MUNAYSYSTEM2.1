d487ec5093446d64c27fd4456436d4ee419708af
jarvishuice <jarvis.realg@gmail.com>
2023-12-29 13:53:54 -0400

    Agregando vista busqueda de productos

--- Front-end/Recepcion/Recepcion/src/views/ESPACIOS.tsx
+++ Front-end/Recepcion/Recepcion/src/views/ESPACIOS.tsx
@@ -1,16 +1,15 @@
 import { Tabs, TabList, Tab, TabPanel } from "@mui/joy";
-import { VisitasTable } from "../controller/Components/VisitasTable";
 
 export function ESPACIOS(){
 return(<Tabs aria-label="Basic tabs" defaultValue={0}>
 <TabList>
-  <Tab>Visitas</Tab>
-  <Tab>Registrar Visita</Tab>
+  <Tab>POS</Tab>
+  <Tab>ORDENES</Tab>
   <Tab>Registrar Visitante</Tab>
   <Tab>Visitantes</Tab>
 </TabList>
 <TabPanel value={0}>
- <VisitasTable/>
+
 </TabPanel>
 <TabPanel value={1}>
 <h1></h1>
