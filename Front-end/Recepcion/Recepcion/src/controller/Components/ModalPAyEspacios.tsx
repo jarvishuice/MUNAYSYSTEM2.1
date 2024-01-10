5085735251a2ce5a6fa6ed1714eff348afd6c537
jarvishuice <jarvis.realg@gmail.com>
2024-01-10 09:13:58 -0400

    agregando documentacion

--- Front-end/Recepcion/Recepcion/src/controller/Components/ModalPAyEspacios.tsx
+++ Front-end/Recepcion/Recepcion/src/controller/Components/ModalPAyEspacios.tsx
@@ -8,20 +8,34 @@ import WorkHistoryIcon from '@mui/icons-material/WorkHistory';
 import { DetallesDeudasEspacios } from './DetalleDeudasEspacios';
 import { Grid } from '@mui/joy';
 import { PagosEspacios } from './pagosEspacios';
-export  function ModalPayEspacios(props:any) {
+/**
+ * `ModalPayEspacios` es un componente React que muestra un modal con detalles de pago.
+ *
+ * @param {any} props - Las propiedades pasadas a este componente. Espera un objeto `deudaCliente`.
+ *
+ * @returns {JSX.Element} Un fragmento de React que contiene un botón y un modal.
+ * Al hacer clic en el botón, se abre el modal que muestra los detalles de pago y deudas.
+ *
+ * El modal contiene dos componentes principales:
+ * 1. `PagosEspacios`: Muestra el formulario para que el cliente pueda pagar. Recibe `props.deudaCliente` como prop.
+ * 2. `DetallesDeudasEspacios`: Muestra los detalles de las deudas del cliente. Recibe `props.deudaCliente` como prop.
+ *
+ * El modal se puede cerrar haciendo clic fuera de él o en el botón de cierre en la esquina superior derecha.
+ */
+export function ModalPayEspacios(props: any): JSX.Element {
   const [open, setOpen] = React.useState<boolean>(false);
 
   return (
     <React.Fragment>
-      <Button color="primary" startDecorator={<WorkHistoryIcon/>}  size="sm"onClick={() => setOpen(true)}>
-       DETALLES
+      <Button color="primary" startDecorator={<WorkHistoryIcon />} size="sm" onClick={() => setOpen(true)}>
+        DETALLES
       </Button>
       <Modal
         aria-labelledby="modal-title"
         aria-describedby="modal-desc"
         open={open}
         onClose={() => setOpen(false)}
-        sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' ,}}
+        sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', }}
       >
         <Sheet
           variant="outlined"
@@ -30,22 +44,22 @@ export  function ModalPayEspacios(props:any) {
             borderRadius: 'md',
             p: 4,
             boxShadow: 'lg',
-            marginTop:12,
-          
+            marginTop: 12,
+
           }}
         >
-            
-            
+
+
           <ModalClose variant="plain" sx={{ m: 1 }} />
-            <Grid style={{ maxHeight: '550px', overflow: 'auto' }} container spacing={2} sx={{ flexGrow: 2}}> 
-            <Grid xs={6}><PagosEspacios cliente={props.deudaCliente}/>
-                    
-                        </Grid>
-                        <Grid xs={5}><DetallesDeudasEspacios deudor={props.deudaCliente}>
-                        </DetallesDeudasEspacios>
-                    
-                        </Grid>
-                        </Grid>
+          <Grid style={{ maxHeight: '550px', overflow: 'auto' }} container spacing={2} sx={{ flexGrow: 2 }}>
+            <Grid xs={6}><PagosEspacios cliente={props.deudaCliente} />
+
+            </Grid>
+            <Grid xs={5}><DetallesDeudasEspacios deudor={props.deudaCliente}>
+            </DetallesDeudasEspacios>
+
+            </Grid>
+          </Grid>
         </Sheet>
       </Modal>
     </React.Fragment>
