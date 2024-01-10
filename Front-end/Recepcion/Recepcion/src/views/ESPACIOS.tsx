32edbba59aa8583d6cac31228e11048cb6e3a81b
jarvishuice <jarvis.realg@gmail.com>
2024-01-10 12:07:21 -0400

    ajustando presentacion

--- Front-end/Recepcion/Recepcion/src/views/ESPACIOS.tsx
+++ Front-end/Recepcion/Recepcion/src/views/ESPACIOS.tsx
@@ -1,23 +1,23 @@
 import { Tabs, TabList, Tab, TabPanel } from "@mui/joy";
 import { POSESPACIOS } from "./POS_ESPACIOS";
-import { ORDENESESPACIOS } from "./OrdenesEspacios";
+//import { ORDENESESPACIOS } from "./OrdenesEspacios";
 import { DeudasEspaciosTable } from "../controller/Components/DeudasEspaciosTable";
 
 export function ESPACIOS(){
 return(<Tabs aria-label="Basic tabs" defaultValue={0}>
 <TabList>
   <Tab>POS</Tab>
-  <Tab>ORDENES</Tab>
-  <Tab>TABLA ORDENES</Tab>
+
+  <Tab>DEUDAS ESPACIOS</Tab>
 
 </TabList>
 <TabPanel value={0}>
 <POSESPACIOS></POSESPACIOS>
 </TabPanel>
-<TabPanel value={1}>
+{/* <TabPanel value={1}>
 <ORDENESESPACIOS></ORDENESESPACIOS>
-</TabPanel>
-<TabPanel value={2}>
+</TabPanel>*/}
+<TabPanel value={1}>
 <DeudasEspaciosTable></DeudasEspaciosTable>
 </TabPanel>
 
